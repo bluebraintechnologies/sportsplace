a05ab71aee71bad843a37eb4d813929dcae1170c
ritesh719 <ritesh.s@henryharvin.in>
2023-01-22 10:27:44 +0530

    initial

---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+	return (
+		<main className={styles.main}>
+
+			<div className={styles.center}>
+				<Image
+					className={styles.logo}
+					src="/black.png"
+					alt="Agway Sports Logo"
+					width={250}
+					height={57}
+					priority
+				/>
+			</div>
+		</main>
+	)
+}
